0287de7e734b12b113aea0ca0ffb5209a899de58
Jyoti Deveda <jyoti@gkmit.co>
2024-11-08 12:41:48 +0530

    feat:[sc-3] add model and migration for orders table

---
+++ src/migrations/20241108045403-create-order.js
@@ -0,0 +1,78 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+      },
+      cart_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'carts',
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'SET NULL',
+      },
+      restaurant_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'restaurants',
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'SET NULL',
+      },
+      delivery_partner_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'CASCADE',
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ['preparing', 'delivered', 'cancelled'],
+        defaultValue: 'preparing',
+      },
+      delivery_charges: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      gst: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      order_charges: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      total_amount: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('orders');
+  },
+};
---
+++ src/models/Order.js
@@ -0,0 +1,68 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {}
+  Order.init(
+    {
+      cart_id: {
+        type: DataTypes.UUID,
+        references: {
+          model: 'Cart',
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'SET NULL',
+      },
+      restaurant_id: {
+        type: DataTypes.UUID,
+        references: {
+          model: 'Restaurant',
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'SET NULL',
+      },
+      delivery_partner_id: {
+        type: DataTypes.UUID,
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'CASCADE',
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: ['preparing', 'delivered', 'cancelled'],
+        defaultValue: 'preparing',
+      },
+      delivery_charges: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      gst: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      order_charges: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      total_amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Order',
+      tableName: 'orders',
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  return Order;
+};
