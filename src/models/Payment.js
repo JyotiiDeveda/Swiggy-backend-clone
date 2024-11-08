6c077fc06a3bf03efeaef3d82f73f84ee587342e
Jyoti Deveda <jyoti@gkmit.co>
2024-11-08 12:51:03 +0530

    feat:[sc-3] add model and migration for payments table

---
+++ src/models/Payment.js
@@ -0,0 +1,40 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Payment extends Model {}
+  Payment.init(
+    {
+      order_id: {
+        type: DataTypes.UUID,
+        references: {
+          model: 'Order',
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'CASCADE',
+      },
+      total_amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.ENUM,
+        values: ['online', 'cash-on-delivery'],
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM,
+        status: ['pending', 'successfull', 'failed'],
+        defaultValue: 'pending',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Payment',
+      tableName: 'payments',
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  return Payment;
+};
