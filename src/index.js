7655f97548ddef9970835a9ced28d9acab34e70f
Jyoti Deveda <jyoti@gkmit.co>
2024-11-11 23:40:47 +0530

    chore:[sc-2] refactor and remove dead code

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ const app = express();
 const { sequelize } = require('./models');
 
 app.use(express.json());
+const PORT = process.env.DB_PORT || 3000;
 
 const startServer = async () => {
-	try {
-		await sequelize.authenticate();
-		console.log('Database connection successfull');
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection successfull');
 
-		const PORT = process.env.DB_PORT || 3000;
-		app.listen(PORT);
-		console.log(`Server running at port ${PORT}`);
-	} catch (err) {
-		console.log('Database connection failed', err);
-	}
+    app.listen(PORT);
+    console.log(`Server running at port ${PORT}`);
+  } catch (err) {
+    console.log('Database connection failed', err);
+  }
 };
 
 startServer();
-
-app.get('/', (req, res) => {
-	res.send('Swiggy-clone server');
-});
