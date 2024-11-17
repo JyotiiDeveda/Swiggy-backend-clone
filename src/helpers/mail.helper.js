2574884684517c3f6d818ffa9e81490f0c8c78b8
Jyoti Deveda <jyoti@gkmit.co>
2024-11-17 16:20:50 +0530

    fix:[sc-19] replace nodemailer configuration from utils to helper

--- src/helpers/mail.helper.js
+++ src/helpers/mail.helper.js
@@ -1,10 +1,34 @@
-const { mailSender } = require('../utils/mail-sender.js');
 const commonHelpers = require('./common.helper.js');
+const nodemailer = require('nodemailer');
+
+const mailSender = async (email, title, body) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  // console.log('Created transporter: ');
+  const mailOptions = {
+    from: process.env.EMAIL || 'Jyoti Deveda',
+    to: email,
+    subject: title,
+    text: body,
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+  // console.log('Mail info: ', info);
+  return info;
+};
 
 const sendVerificationEmail = async (email, title, body) => {
   try {
     await mailSender(email, title, body);
-    console.log('Email sent successfully: ');
+    // console.log('Email sent successfully: ');
   } catch (error) {
     console.log('Error occurred while sending email: ', error);
     throw commonHelpers.customError('Mail not send', 400);
