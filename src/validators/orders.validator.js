c956a080cdc1b9d290b4aa484fbc03108b4cca5f
Jyoti Deveda <jyoti@gkmit.co>
2024-11-19 14:42:08 +0530

    fix:[sc-19] add order validators

--- src/validators/orders.validator.js
+++ src/validators/orders.validator.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const commonHelper = require('../helpers/common.helper');
+const constants = require('../constants/constants');
 
 const validatePlaceOrderSchema = (req, res, next) => {
   try {
@@ -37,6 +38,68 @@ const validatePlaceOrderSchema = (req, res, next) => {
   }
 };
 
+const validateDeliveryPartnerId = (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      userId: Joi.string()
+        .guid({
+          version: 'uuidv4',
+        })
+        .required(),
+    });
+
+    const { error, value } = schema.validate(req.body);
+
+    if (error) {
+      console.log(error);
+      const errMsg =
+        error.details
+          .map(detail => detail.message)
+          .join(', ')
+          .replaceAll(`"`, '') || 'Delivery partner id validation failed';
+
+      return commonHelper.customErrorHandler(res, errMsg, 422);
+    }
+
+    req.body = value;
+    next();
+  } catch (err) {
+    console.log('Error validating input fields: ', err);
+    return commonHelper.customErrorHandler(res, err.message, 400);
+  }
+};
+
+const validateOrderStatus = (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      status: Joi.string()
+        .required()
+        .valid(...Object.values(constants.ORDER_STATUS)),
+    });
+
+    const { error, value } = schema.validate(req.body);
+
+    if (error) {
+      console.log(error);
+      const errMsg =
+        error.details
+          .map(detail => detail.message)
+          .join(', ')
+          .replaceAll(`"`, '') || 'Delivery partner id validation failed';
+
+      return commonHelper.customErrorHandler(res, errMsg, 422);
+    }
+
+    req.body = value;
+    next();
+  } catch (err) {
+    console.log('Error validating input fields: ', err);
+    return commonHelper.customErrorHandler(res, err.message, 400);
+  }
+};
+
 module.exports = {
   validatePlaceOrderSchema,
+  validateDeliveryPartnerId,
+  validateOrderStatus,
 };
