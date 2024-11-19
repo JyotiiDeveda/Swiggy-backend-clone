b6d5f52e4cc78e5c7ab6bea14ca276570f147f09
Jyoti Deveda <jyoti@gkmit.co>
2024-11-20 00:21:20 +0530

    fix:[sc-21] added custom error handler in payments routes from controllers

--- src/controllers/payments.controller.js
+++ src/controllers/payments.controller.js
@@ -1,24 +1,31 @@
 const commonHelper = require('../helpers/common.helper');
 const paymentServices = require('../services/payments.service');
 
-const makePayment = async (req, res) => {
+const makePayment = async (req, res, next) => {
   try {
     const currentUser = req.user;
     const payload = req.body;
     const payment = await paymentServices.makePayment(currentUser, payload);
-    return commonHelper.customResponseHandler(res, 'Payment successfull', 201, payment);
+    res.statusCode = 201;
+    res.data = payment;
+    res.message = 'Payment successfull';
+    next();
   } catch (err) {
     console.log('Error in making payment: ', err);
     return commonHelper.customErrorHandler(res, err.message, err.statusCode);
   }
 };
 
-const getAll = async (req, res) => {
+const getAll = async (req, res, next) => {
   try {
     const { userId } = req.user;
     const { page, limit } = req.query;
     const allPayments = await paymentServices.getAllPayments(userId, page, limit);
-    return commonHelper.customResponseHandler(res, 'Fetched all payments successfully', 201, allPayments);
+    res.statusCode = 200;
+    res.data = allPayments;
+    res.message = 'Fetched all payments successfully';
+
+    next();
   } catch (err) {
     console.log('Error in getting payment: ', err);
     return commonHelper.customErrorHandler(res, err.message, err.statusCode);
--- src/routes/payments.route.js
+++ src/routes/payments.route.js
@@ -1,14 +1,22 @@
 const router = require('express').Router();
 const authMiddlewares = require('../middlewares/auth.middleware');
 const paymentsController = require('../controllers/payments.controller');
+const commonHelpers = require('../helpers/common.helper');
 
 router.post(
   '/',
   authMiddlewares.authenticateToken,
   authMiddlewares.isAuthorized,
-  paymentsController.makePayment
+  paymentsController.makePayment,
+  commonHelpers.customResponseHandler
 );
 
-router.get('/', authMiddlewares.authenticateToken, authMiddlewares.isAuthorized, paymentsController.getAll);
+router.get(
+  '/',
+  authMiddlewares.authenticateToken,
+  authMiddlewares.isAuthorized,
+  paymentsController.getAll,
+  commonHelpers.customResponseHandler
+);
 
 module.exports = router;
