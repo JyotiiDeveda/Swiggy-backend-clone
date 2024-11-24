7ae56f0165ca945b49e419e07a24efd365ff138c
Jyoti Deveda <jyoti@gkmit.co>
2024-11-24 13:52:04 +0530

    feat:[sc-28] add apis to work with roles

---
+++ src/serializers/roles.serializer.js
@@ -0,0 +1,25 @@
+const serializeRoles = (req, res, next) => {
+  let rows = res.data?.rows || res.data;
+
+  const isSingleItem = !Array.isArray(rows);
+
+  if (!Array.isArray(rows)) {
+    rows = [rows];
+  }
+
+  const roles = rows.map(role => {
+    const roleData = {
+      id: role?.id,
+      name: role?.name,
+    };
+    return roleData;
+  });
+
+  res.data = isSingleItem ? { role: roles[0] } : { roles };
+
+  next();
+};
+
+module.exports = {
+  serializeRoles,
+};
