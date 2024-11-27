db4ab61197f84118c15f43c09ce90ce66e6926e3
Jyoti Deveda <jyoti@gkmit.co>
2024-11-27 14:31:42 +0530

    fix:[sc-36] fix transaction, testcases and for payments

--- src/services/payments.service.js
+++ src/services/payments.service.js
@@ -2,70 +2,58 @@ const commonHelpers = require('../helpers/common.helper');
 const constants = require('../constants/constants');
 const paymentsHelper = require('../helpers/payments.helper');
 const { Payment, Order, Cart } = require('../models');
-const { sequelize } = require('../models');
 const mailHelper = require('../helpers/mail.helper');
 
 const makePayment = async (currentUser, payload) => {
-  const transactionContext = await sequelize.transaction();
-  try {
-    const { userId, email } = currentUser;
-    const { orderId, type } = payload;
+  const { userId, email } = currentUser;
+  const { orderId, type } = payload;
 
-    // check if an unplaced order exists
-    const orderExists = await Order.findOne({
+  // check if an unplaced order exists
+  const orderExists = await Order.findOne({
+    where: {
+      id: orderId,
+    },
+    include: {
+      model: Cart,
       where: {
-        id: orderId,
-      },
-      include: {
-        model: Cart,
-        where: {
-          user_id: userId,
-        },
-      },
-    });
-
-    if (!orderExists) {
-      throw commonHelpers.customError('Order not found', 404);
-    }
-
-    // ensure payment is not done already
-    const payment = await Payment.findOne({
-      where: { order_id: orderId, user_id: userId },
-    });
-
-    if (payment) {
-      throw commonHelpers.customError('Payment done already', 409);
-    }
-
-    const totalCost = orderExists.total_amount;
-    const response = await paymentsHelper.pay(totalCost);
-
-    if (!response) throw commonHelpers.customError('Payment failed', 400);
-
-    const newPayment = await Payment.create(
-      {
         user_id: userId,
-        order_id: orderId,
-        total_amount: totalCost,
-        type,
-        status: constants.PAYMENT_STATUS.SUCCESSFUL,
       },
-      { transaction: transactionContext }
-    );
+    },
+  });
 
-    if (!newPayment) {
-      throw commonHelpers.customError('Error in payment', 400);
-    }
-
-    await mailHelper.sendPaymentStatusMail(email, orderId, newPayment);
-    await transactionContext.commit();
-
-    return newPayment;
-  } catch (err) {
-    await transactionContext.rollback();
-    console.log('Error in making Payment', err.message);
-    throw commonHelpers.customError(err.message, err.statusCode);
+  if (!orderExists) {
+    throw commonHelpers.customError('Order not found', 404);
   }
+
+  // ensure payment is not done already
+  const payment = await Payment.findOne({
+    where: { order_id: orderId, user_id: userId },
+  });
+
+  if (payment) {
+    throw commonHelpers.customError('Payment done already', 409);
+  }
+
+  const totalCost = orderExists.total_amount;
+  const response = await paymentsHelper.pay(totalCost);
+
+  if (!response) throw commonHelpers.customError('Payment failed', 400);
+
+  const newPayment = await Payment.create({
+    user_id: userId,
+    order_id: orderId,
+    total_amount: totalCost,
+    type,
+    status: constants.PAYMENT_STATUS.SUCCESSFUL,
+  });
+
+  if (!newPayment) {
+    throw commonHelpers.customError('Error in payment', 400);
+  }
+
+  mailHelper.sendPaymentStatusMail(email, orderId, newPayment);
+
+  return newPayment;
 };
 
 module.exports = { makePayment };
--- tests/services/payments.service.test.js
+++ tests/services/payments.service.test.js
@@ -1,5 +1,4 @@
 const { makePayment } = require('../../src/services/payments.service');
-const { sequelize } = require('../../src/models');
 const models = require('../../src/models');
 const mailHelper = require('../../src/helpers/mail.helper');
 // const constants = require('../../src/constants/constants');
@@ -13,7 +12,6 @@ jest.mock('../../src/helpers/common.helper');
 jest.mock('../../src/helpers/payments.helper');
 
 describe('Payments Service Tests', () => {
-  let transactionMock;
   let currentUser;
   let payload;
   let orderId;
@@ -28,13 +26,6 @@ describe('Payments Service Tests', () => {
     orderId = faker.string.uuid();
     // email = faker.internet.email();
     payload = { orderId, type: 'CREDIT_CARD' };
-
-    transactionMock = {
-      commit: jest.fn(),
-      rollback: jest.fn(),
-    };
-
-    sequelize.transaction = jest.fn().mockResolvedValue(transactionMock);
 
     // Mock the models and other dependencies
     models.Order.findOne = jest.fn();
@@ -69,14 +60,12 @@ describe('Payments Service Tests', () => {
       const result = await makePayment(currentUser, payload);
 
       expect(result).toHaveProperty('id');
-      expect(transactionMock.commit).toHaveBeenCalled();
     });
 
     it('should throw error if order not found', async () => {
       models.Order.findOne.mockResolvedValue(null); // Order not found
 
       await expect(makePayment(currentUser, payload)).rejects.toThrowError('Order not found');
-      expect(transactionMock.rollback).toHaveBeenCalled();
     });
 
     it('should throw error if payment already made for the order', async () => {
@@ -85,7 +74,6 @@ describe('Payments Service Tests', () => {
       models.Payment.findOne.mockResolvedValue(existingPayment); // Payment already exists
 
       await expect(makePayment(currentUser, payload)).rejects.toThrowError('Payment done already');
-      expect(transactionMock.rollback).toHaveBeenCalled();
     });
 
     it('should throw error if payment fails', async () => {
@@ -95,7 +83,6 @@ describe('Payments Service Tests', () => {
       paymentsHelper.pay.mockResolvedValue(false); // Payment failed
 
       await expect(makePayment(currentUser, payload)).rejects.toThrowError('Payment failed');
-      expect(transactionMock.rollback).toHaveBeenCalled();
     });
 
     it('should throw error if payment creation fails', async () => {
@@ -106,7 +93,6 @@ describe('Payments Service Tests', () => {
       models.Payment.create.mockResolvedValue(null); // Payment creation failed
 
       await expect(makePayment(currentUser, payload)).rejects.toThrowError('Error in payment');
-      expect(transactionMock.rollback).toHaveBeenCalled();
     });
   });
 });
